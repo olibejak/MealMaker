f6ac68a35397cc38dfc437d2bc84b89a81ab9d74
Jakub Oliberius <olibejak@fel.cvut.cz>
2024-04-20 16:11:06 +0200

    Recipe screen api display

--- assets/icons.js
+++ assets/icons.js
@@ -1,4 +1,4 @@
-import Svg, {G, Path, Rect} from "react-native-svg";
+import Svg, {G, Path} from "react-native-svg";
 
 const HamburgerIcon = () =>(
     <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -196,7 +196,7 @@ const ArrowDropDown = () => (
 )
 const EditIcon = () => (
     <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <Path fill-rule="evenodd" clip-rule="evenodd"
+        <Path fillRule="evenodd" clipRule="evenodd"
               d="M19.06 3.59L20.41 4.94C21.2 5.72 21.2 6.99 20.41 7.77L7.18 21H3V16.82L13.4 6.41L16.23 3.59C17.01 2.81 18.28 2.81 19.06 3.59ZM5 19L6.41 19.06L16.23 9.23L14.82 7.82L5 17.64V19Z"
               fill="#49454F"/>
     </Svg>
--- screens/RecipesScreen.js
+++ screens/RecipesScreen.js
@@ -1,98 +1,98 @@
-import {View, StyleSheet,ScrollView} from "react-native";
+import {View, StyleSheet, ScrollView, ActivityIndicator, FlatList} from "react-native";
 import TopNavigationBar from "../components/TopNavigationBar";
 import BottomNavigationBar from "../components/BottomNavigationBar";
 import RecipeCard from "../components/RecipeCard";
 import SearchBar from "../components/SearchBar";
 import {BookIcon, HamburgerIcon} from "../assets/icons";
-import {useNavigation} from "@react-navigation/native";
+import React, {useEffect, useState} from "react";
 
-export default function RecipesScreen () {
+export default function RecipesScreen ({navigation}) {
     const title = "Recipes";
     const filtersOn = false;
     const selectedBottomBar = "Ingredients";
-    const navigation = useNavigation();
-    const state = navigation.getState();
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [recipes, setRecipes] = useState([]);
+    const [displayedRecipes, setDisplayedRecipes] = useState([]);
 
-    const recipes = [
-        {
-            id: '1',
-            title: 'My first recipe',
-            date: '10.3.2024',
-            description: 'Dear Diary,\n' +
-                '\n' +
-                'Today, I embarked on a culinary adventure, one that I\'ve been planning for weeks: making Chicken Fajita Mac and Cheese from scratch! I must confess, the fusion of spicy fajitas with the creamy, comforting embrace of mac and cheese felt like a gastronomic quest worth penning down.\n' +
-                '\n' +
-                'My kitchen transformed into a vibrant stage as I began. First, I sautéed onions and bell peppers until they were just soft, their sizzling a song of the feast to come. I added strips of seasoned chicken breast, watching them brown, infused with the robust flavors of cumin, paprika, and a hint of lime - a zesty ode to the fajitas\' spirit.\n' +
-                '\n' +
-                'Once the chicken was tender and the vegetables were imbued with charred edges, I stirred in a roux, that magical blend of flour and butter, to cradle my concoction in a thick, velvety sauce. The rich aroma of simmering chicken stock filled the air as I poured it in, and I found myself dancing to the rhythm of the bubbling pot.\n' +
-                '\n' +
-                'And then, the cheese - oh, the cheese! Generous handfuls of cheddar melted into the mix, stretching into strings of golden perfection as I stirred. I couldn\'t resist sneaking a taste, and Diary, it was divine - a creamy symphony that promised indulgence.\n' +
-                '\n' +
-                'I folded the al dente macaroni into the saucy chicken mixture, the pasta absorbing the bold, savory flavors. The final touch was a sprinkle of fresh cilantro and a squeeze of lime, adding a pop of freshness that whispered of summer evenings and laughter.\n' +
-                '\n' +
-                'As the dish baked to a bubbly, golden-topped delight, I set the table, lit candles, and poured a glass of chilled white wine. The first bite was a revelation - a harmonious blend of textures and tastes, each forkful a story of comfort food meeting the zesty kick of Tex-Mex.\n' +
-                '\n' +
-                'I must say, cooking Chicken Fajita Mac and Cheese wasn\'t just about the flavors; it was about creating a moment, a delicious memory that I\'d look back on with a smile. And with that, I close today\'s entry, with a heart as full as my satisfied stomach.',
-            image: require('../assets/testing_images/recipe.jpg'),
-        },
-        {
-            id: '2',
-            title: 'My second recipe',
-            date: '12.3.2024',
-            description: 'Dear Diary,\n' +
-                '\n' +
-                'This evening\'s culinary soiree took me down the charming alleys of Italy with a hearty twist: Spinach and Ricotta Stuffed Shells. Preparing this dish felt like weaving a tapestry of flavors, each more vivid than the last.\n' +
-                '\n' +
-                'I started by boiling large pasta shells to al dente perfection, their hollow shapes waiting eagerly to be filled with treasures. For the filling, I mixed fresh ricotta with spinach, sautéed garlic, and nutmeg, creating a lush green mixture that promised bursts of creamy delight.\n' +
-                '\n' +
-                'Filling each shell was akin to painting a canvas, each stroke filled with intention and love. I nestled them snugly in a bed of homemade tomato sauce, its simmering tomatoes blending with basil and a touch of oregano, whispering tales of sun-kissed Italian gardens.\n' +
-                '\n' +
-                'A generous topping of mozzarella promised a molten crown, turning golden and bubbly in the oven\'s embrace. The aroma of baking cheese and herbs filled my kitchen, a prelude to the feast.\n' +
-                '\n' +
-                'As I served the shells, each laden with rich fillings and cloaked in tomato sauce, it was a celebration of textures and flavors, each bite a testament to the simple joys of Italian cooking.\n' +
-                '\n' +
-                'Tonight was not just about savoring a meal; it was about savoring life\'s delicious moments, crafting memories that linger long after the last bite. Diary, this is one recipe that will surely find its way into many more evenings filled with joy and laughter.',
-            image: require('../assets/testing_images/recipe.jpg'),
-        },
-        {
-            id: '3',
-            title: 'My third recipe',
-            date: '14.3.2024',
-            description: 'Dear Diary,\n' +
-                '\n' +
-                'Today\'s kitchen escapade was a dive into the heartwarming world of comfort food with a Southern twist: Pulled Pork Sliders. From the slow roasting of the pork to the tangy crunch of homemade coleslaw, each step was a dance of flavors.\n' +
-                '\n' +
-                'The pork shoulder bathed in a marinade of brown sugar, smoked paprika, and a dash of chili, slow-cooked to tender perfection. As it cooked, its aroma wafted through the house, a beckoning call to the table.\n' +
-                '\n' +
-                'I shredded the pork, its fibers soaking up the spicy, sweet juices, and piled it high on freshly baked buns. A scoop of coleslaw, crisp and zesty with its cider vinegar dressing, topped each slider, adding a refreshing contrast.\n' +
-                '\n' +
-                'As we bit into the sliders, the combination of soft, succulent pork and the sharp, crunchy slaw created a symphony of textures and tastes. It was more than a meal; it was a celebration, a gathering of friends and flavors around my humble table.\n' +
-                '\n' +
-                'These sliders were not just food; they were tiny, delicious symbols of togetherness and joy, a reminder that sometimes, the simplest dishes can stir the deepest emotions. With that thought, I end today\'s entry, grateful for the simple pleasures that fill my life and my palate.',
-            image: require('../assets/testing_images/recipe.jpg'),
+    useEffect(() => {
+        const fetchRecipesFromAPI = async () => {
+            for (let i = 97; i <= 122; ++i) {
+                const char = String.fromCharCode(i);
+                let url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${char}`;
+                try {
+                    const response = await fetch(url);
+                    const json = await response.json();
+                    if (json.meals) {
+                        setRecipes(prevRecipes =>
+                            [...prevRecipes, ...json.meals]);
+                        }
+                    }
+            catch (error) {
+                    console.error("Failed to fetch ingredients or request timed out:", error);
+                }
+            }
+        };
+        fetchRecipesFromAPI();
+    }, []);
+
+    const loadMoreRecipes = () => {
+        if (currentIndex < recipes.length) {
+            const newIndex = currentIndex + 10;
+            const newItems = recipes.slice(currentIndex, newIndex);
+            setDisplayedRecipes([...displayedRecipes, ...newItems]);
+            setCurrentIndex(newIndex);
         }
-    ];
+    };
+
+    const renderItem = ({item}) => {
+        const ingredientKeys = Object.keys(item).filter(key => key.startsWith('strIngredient'));
+        const amountKeys = Object.keys(item).filter(key => key.startsWith('strMeasure'));
+
+        const ingredientsList = ingredientKeys.map((key, index) => {
+            const ingredient = item[key];
+            const amount = item[amountKeys[index]];
+
+            // Check if ingredient is not null or empty string
+            if (ingredient && ingredient.trim() !== '') {
+                return `${amount} ${ingredient}`;
+            }
+            return null; // Filter out null or empty ingredient
+        }).filter(item => item !== null).join(', ');
+
+        return (
+            <RecipeCard
+                title={item.strMeal}
+                date={item.strTags}
+                description={ingredientsList}
+                image={item.strMealThumb}
+                onPressDetails={() => navigation.navigate("RecipeDetails", {recipe: item})}
+                onPressSecondary={() => navigation.navigate("StepByStepRecipe", {recipe: item})}
+                actionButton={'cook'}
+            />
+        )
+    }
 
     return (
         <View style={styles.screen}>
             <View>
                 <TopNavigationBar title={title} LeftIcon={HamburgerIcon} RightIcon={BookIcon} />
             </View>
-            <ScrollView style={styles.scrollableScreen} contentContainerStyle={styles.scrolling}>
-                <SearchBar filtersOn={filtersOn} />
-                {recipes.map((recipe) => (
-                    <RecipeCard
-                        key={recipe.id}
-                        title={recipe.title}
-                        date={recipe.date}
-                        description={recipe.description}
-                        image={recipe.image}
-                        onPressDetails={() => console.log('Details pressed for recipe', recipe.id)}
-                        onPressCook={() => console.log('Cook pressed for recipe', recipe.id)}
-                        actionButton={'cook'}
-                    />
-                ))}
-            </ScrollView>
+            <FlatList
+                data={recipes}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+                ListHeaderComponent={<SearchBar filtersOn={filtersOn}/>}
+                style={styles.scrollableScreen}
+                contentContainerStyle={styles.scrolling}
+                ListEmptyComponent={
+                    <View style={styles.loadingScreen}>
+                        <ActivityIndicator size="large" />
+                    </View>
+                }
+                onEndReached={loadMoreRecipes}
+                onEndReachedThreshold={0.7}
+                initialNumToRender={10}
+            />
             <View>
                 <BottomNavigationBar selected={selectedBottomBar} />
             </View>
