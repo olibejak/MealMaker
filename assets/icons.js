7d5d2afbb672b88839e2e28a61fec5590c2411ae
Jakub Oliberius <olibejak@fel.cvut.cz>
2024-04-29 21:13:29 +0200

    filled star icon last edit

--- assets/icons.js
+++ assets/icons.js
@@ -175,10 +175,10 @@ const StarOutlineIcon = () => (
     </Svg>
 )
 const StarFilledIcon = () => (
-    <Svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-        <Path d="M0 0h24v24H0z" fill="none"/>
-        <Path d="M0 0h24v24H0z" fill="none"/>
-        <Path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+    <Svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <Path
+            d="M10 15.77L16.18 19.5L14.54 12.47L20 7.74L12.81 7.13L10 0.5L7.19 7.13L0 7.74L5.46 12.47L3.82 19.5L10 15.77Z"
+            fill="black"/>
     </Svg>
 )
 const TimerIcon = () => (
